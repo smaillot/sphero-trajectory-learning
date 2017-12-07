9a7f71acd48c7ea7c64408b06e6571f7abec2c8e
Stéphane Maillot <s.maillot2@gmail.com>
2017-12-07 18:20:36 +0100

    Creating manual DMP control for imitation

--- imitation/gen_points.m
+++ imitation/gen_points.m
@@ -1,10 +1,10 @@
 function dat=gen_points(resolution,data)
     timec=gen_time(resolution,data);
-    dat.x=interp1(data.times,data.x,timec);
-    dat.y=interp1(data.times,data.y,timec);
-    dat.vx=interp1(data.times,data.vx,timec);
-    dat.vy=interp1(data.times,data.vy,timec);
-    dat.ax=interp1(data.times,data.ax,timec);
-    dat.ay=interp1(data.times,data.ay,timec);
+    dat.y_xr=interp1(data.times,data.y_xr,timec);
+    dat.y_yr=interp1(data.times,data.y_yr,timec);
+    dat.yd_xr=interp1(data.times,data.yd_xr,timec);
+    dat.yd_yr=interp1(data.times,data.yd_yr,timec);
+    dat.ydd_xr=interp1(data.times,data.ydd_xr,timec);
+    dat.ydd_yr=interp1(data.times,data.ydd_yr,timec);
     dat.times=timec;
 end
