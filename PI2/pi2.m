9d0713b3801d75602012ad3cc8475030a62e9d51
Lagerbe <30956718+Lagerbe@users.noreply.github.com>
2017-12-07 17:48:48 +0100

    pasfini

--- PI2/pi2.m
+++ PI2/pi2.m
@@ -5,14 +5,17 @@ function theta = pi2(r, phi, a, g, sigma, theta_i, K)
     % g : basis function from the system dynamics, 
     % sigma : variance of the mean-zero noise
     % theta_i : initial parameter vector, 
-    % K : number of roll-outs per update
+    % K : number of roll-outs per update,
+    % init_pos: initial osition of the robot
     tolerance = 1e-5;
     R_last = -1;
     R = 0;
     gamma = 1;
+    K=5;
     
     while abs(R - R_last) > tolerance
         
+        data=rollout(K, init_pos, sigma);
         for k=1:K
            
             eps = normrnd(0,gamma*sigma,size(theta_i));
