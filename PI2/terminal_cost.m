49caaef6f3db33735b81b3b835835931216c9f46
Stéphane Maillot <s.maillot2@gmail.com>
2017-12-07 23:58:27 +0100

    Merge PI2 branch

---
+++ PI2/terminal_cost.m
@@ -0,0 +1,10 @@
+function psi_N=terminal_cost(final_point,goal_point, penalization)
+    %This function takes as an entry the the final point of the roll-out
+    % and the goal both in the form (x;y). 
+    % The output is a radial penalization of the distance between the final
+    % point of the roll out and the goal
+    d=norm(final_point-goal_point);
+    psi_N=penalization*d;
+end
+    
+    
