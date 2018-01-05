b8e92e988d04d015eedbc8fb1f62228ea8028f99
Stéphane Maillot <s.maillot2@gmail.com>
2018-01-05 12:34:11 +0100

    update basis function

--- PI2/basis_function.m
+++ PI2/basis_function.m
@@ -1,7 +1,7 @@
 function [ g ] = basis_function( r, rec )
 addpath('../DMP-LWR')
     g = [];
-    for j=1:size(rec, 2)
+    for j=1:length(rec.times)
         g = cat(3, g, basis_function_time(r, rec, j));
     end
 end
@@ -10,16 +10,15 @@ function [ g ] = basis_function_time( r, rec, j )
 %BASIS_FUNCTION_TIME return p sampled approximation of f given its related
 % DMP r, as defined in dmpTrain and its states rec (x;y;t) according to
 % time sample j
-    state = rec(1:2,i);
-    w = weight_calc(r, i); % compute weights at time sample i
-    psum_x=0; 
+    state = [rec.x;rec.y];
+    psum_x=[]; 
     pdiv_x=0;
-    psum_y=0; 
+    psum_y=[]; 
     pdiv_y=0;
     for i=1:r.ng
-        psum_x=psum_x+psiF(r.h, r.c, r.stime(j)/r.d1,i)*r.w_x(i);
+        psum_x(i)=psiF(r.h, r.c, r.stime(j)/r.d1,i)*r.w_x(i);
         pdiv_x=pdiv_x+psiF(r.h, r.c, r.stime(j)/r.d1,i);
-        psum_y=psum_y+psiF(r.h, r.c, r.stime(j)/r.d1,i)*r.w_y(i);
+        psum_y(i)=psiF(r.h, r.c, r.stime(j)/r.d1,i)*r.w_y(i);
         pdiv_y=pdiv_y+psiF(r.h, r.c, r.stime(j)/r.d1,i);
     end
     g = [(psum_x/pdiv_x)*r.stime(j)*(r.gx-r.x0);(psum_y/pdiv_y)*r.stime(j)*(r.gy-r.y0)]; 
