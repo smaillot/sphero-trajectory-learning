9bb0ce4a5e734f1eb9ac42e58aa47d50c705f21c
Lagerbe <30956718+Lagerbe@users.noreply.github.com>
2018-01-06 23:19:26 +0100

    Begining to put order
    It is time to link all the functions (It is not finished yet)
    Some error correction too

--- PI2/Test_PI2.m
+++ PI2/Test_PI2.m
@@ -10,49 +10,58 @@ addpath(['../DMP-LWR']);
 % This function take the number of points in which we want our trajectory
 % to pass throgh
 
-desPath=genDesiredTrajectory(2,30)
+desPath=genDesiredTrajectory(2,6)
 
-%% Parameters of the DMP 
+%% Determine parameters of the DMP
 
-dlg_title="choose parameter";
-prompt={'K, Stifness coefficient', 'D, Damping coefficient', 'ng, number of gaussian', 'as, decrease of the time dx/dt = as*x','s, initial time'};
-default={'5000','500','100','1','1'};
-uiwait(msgbox('Choose parameter','Parameter','modal'))
-num_lines = 1;
-answer= inputdlg(prompt,dlg_title,num_lines, default);
-par=generate_param(answer);
-
-g=gen_dmp_param(par,desPath);
-
-%% Initial weights of the basis function
-dlg_title="choose initial weights of your basis functions";
-a='Enter your vector of weights [ ; ; ;... ; ] (';
-b='times)';
-prompt={cat(2,a,num2str(g.ng),b)};
-uiwait(msgbox('Choose initial weights','Parameter','modal'))
-num_lines = 1;
-answer= inputdlg(prompt,dlg_title,num_lines, default);
-theta_i=str2num(answer);
-
-%% Initial Position
-
-init_pos=[desPath.x(1);desPath.y(1)];
-
-%% Other initialization
-param.K=g.K;
-param.tau=g.tau;
-param.D=g.D;
-param.times=g.times;
-
-gamma=0.95;
-
-nb_rollout=5;
-
-%% Stephane, you need to initialize this
-r=0;
-phi=0;
-a=0;
+input_struct=init_PI2_learning()
 
 
-%% Launch the learning
-theta = pi2(param,r, phi, a, g, sigma, theta_i, nb_rollout, init_pos, gamma)
+
+
+
+
+% %% Parameters of the DMP 
+% 
+% dlg_title="choose parameter";
+% prompt={'K, Stifness coefficient', 'D, Damping coefficient', 'ng, number of gaussian', 'as, decrease of the time dx/dt = as*x','s, initial time'};
+% default={'5000','500','100','1','1'};
+% uiwait(msgbox('Choose parameter','Parameter','modal'))
+% num_lines = 1;
+% answer= inputdlg(prompt,dlg_title,num_lines, default);
+% par=generate_param(answer);
+% 
+% g=gen_dmp_param(par,desPath);
+% 
+% %% Initial weights of the basis function
+% dlg_title="choose initial weights of your basis functions";
+% a='Enter your vector of weights [ ; ; ;... ; ] (';
+% b='times)';
+% prompt={cat(2,a,num2str(g.ng),b)};
+% uiwait(msgbox('Choose initial weights','Parameter','modal'))
+% num_lines = 1;
+% answer= inputdlg(prompt,dlg_title,num_lines, default);
+% theta_i=str2num(answer);
+% 
+% %% Initial Position
+% 
+% init_pos=[desPath.x(1);desPath.y(1)];
+% 
+% %% Other initialization
+% param.K=g.K;
+% param.tau=g.tau;
+% param.D=g.D;
+% param.times=g.times;
+% 
+% gamma=0.95;
+% 
+% nb_rollout=5;
+% 
+% %% Stephane, you need to initialize this
+% r=0;
+% phi=0;
+% a=0;
+% 
+% 
+% %% Launch the learning
+% theta = pi2(param,r, phi, a, g, sigma, theta_i, nb_rollout, init_pos, gamma)
